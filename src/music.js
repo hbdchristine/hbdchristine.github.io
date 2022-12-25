cf362a04b5ab411636a5f8577dff61487b02c7fb
Sathira Katugaha <SGKat@SathBook-Pro.local>
2022-12-25 00:00:58 -0500

    fix music controls

--- src/music.js
+++ src/music.js
@@ -1,6 +1,7 @@
 const playPauseButton = document.getElementById("playPause");
 const playPauseIcon = document.getElementById("playPauseIcon");
 const playPauseText = document.getElementById("playPauseText");
+const musicElement = document.getElementById("music");
 
 const playSrc = "./assets/play.png";
 const pauseSrc = "./assets/pause.png";
@@ -8,26 +9,20 @@ const pauseSrc = "./assets/pause.png";
 const playText = "Play Music";
 const pauseText = "Pause Music ";
 
-function handleMusicButtonSync() {
-  const musicElement = document.getElementById("music");
+musicElement.addEventListener("play", function () {
+  playPauseIcon.src = pauseSrc;
+  playPauseText.innerHTML = pauseText;
+});
 
-  if (musicElement.paused) {
-    playPauseIcon.src = playSrc;
-    playPauseText.innerHTML = playText;
-  } else {
-    playPauseIcon.src = pauseSrc;
-    playPauseText.innerHTML = pauseText;
-  }
-}
+musicElement.addEventListener("pause", function () {
+  playPauseIcon.src = playSrc;
+  playPauseText.innerHTML = playText;
+});
 
 playPauseButton.addEventListener("click", function () {
-  const musicElement = document.getElementById("music");
   if (musicElement.paused) {
     musicElement.play();
   } else {
     musicElement.pause();
   }
-  handleMusicButtonSync();
 });
-
-document.addEventListener("DOMContentLoaded", handleMusicButtonSync);
